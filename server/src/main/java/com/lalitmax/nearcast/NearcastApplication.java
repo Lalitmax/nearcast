0f0b40b6a9dd4863689399100967923de7cfa706
ddnserver <ddnstorage@gmail.com>
2025-11-01 22:41:37 +0000

    Initial commit just added server folder init

---
+++ server/src/main/java/com/lalitmax/nearcast/NearcastApplication.java
@@ -0,0 +1,13 @@
+package com.lalitmax.nearcast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NearcastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NearcastApplication.class, args);
+	}
+
+}
